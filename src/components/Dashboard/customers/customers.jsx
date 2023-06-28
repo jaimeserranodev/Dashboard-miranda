971e164dd82d3c3709d4ab99a5095f8201aaa807
jaimeserranodev <jaime.serrano.dev@gmail.com>
2023-06-28 14:43:13 +0100

    terminado en el home widget y customers

--- src/components/Dashboard/customers/customers.jsx
+++ src/components/Dashboard/customers/customers.jsx
@@ -1,17 +1,60 @@
 import React from 'react'
+import "./customers.css"
+import perfil from '../../../imagenes/foto.perfil.jpeg';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
 
 const Customers = () => {
     return (
         <div className='customers'>
-            <span>Latest Review by Customers</span>
-            <div className="top"> 
-                
+                <span className='customers-review'>Latest Review by Customers</span>
+                <div className="customer-container">
+                <div className="wrapper-top-botton">
+                    <div className="top"> 
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</span>
+                    </div>
+                    
+                    <div className="botton">
+                        <img id="review-foto" src={perfil}/>
+                        <div className="botton-text">
+                            <span>Kusnaidi Anderson</span>
+                            <span className='subtitle'>4 min ago</span>
+                        </div>
+                        <span><AiOutlineCheckCircle /></span>
+                        <span><TiDeleteOutline /></span>
+                    </div>
+                </div>
+                <div className="wrapper-top-botton">
+                    <div className="top"> 
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</span>
+                    </div>
+                    
+                    <div className="botton">
+                        <img id="review-foto" src={perfil}/>
+                        <div className="botton-text">
+                            <span>Kusnaidi Anderson</span>
+                            <span className='subtitle'>4 min ago</span>
+                        </div>
+                        <span><AiOutlineCheckCircle /></span>
+                        <span><TiDeleteOutline /></span>
+                    </div>
+                </div>
+                <div className="wrapper-top-botton">
+                    <div className="top"> 
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</span>
+                    </div>
+                    
+                    <div className="botton">
+                        <img id="review-foto" src={perfil}/>
+                        <div className="botton-text">
+                            <span>Kusnaidi Anderson</span>
+                            <span className='subtitle'>4 min ago</span>
+                        </div>
+                        <span><AiOutlineCheckCircle /></span>
+                        <span><TiDeleteOutline /></span>
+                    </div>
+                </div>
             </div>
-            <div className="botton">
-                <span className='number'></span>
-                <span className='subtitle'></span>
-            </div>
-
         </div>
     )
 }
